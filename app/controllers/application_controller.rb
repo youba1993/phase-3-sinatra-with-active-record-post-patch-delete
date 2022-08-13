d2edb350f9ac52ec0aaf04b7493c5bd62e790447
youbaidjis <youba.etud@gmail.com>
2022-08-13 00:21:26 -0700

    commit tree 9a99bd558844f67088e7491268a201c3bdc9e6e9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,33 @@ class ApplicationController < Sinatra::Base
     })
   end
 
+  delete '/reviews/:id' do 
+    # find the review using the ID
+  review = Review.find(params[:id])
+  # delete the review
+  review.destroy
+  # send a response with the deleted review as JSON
+  review.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+
+
 end
